646db4889a7855a5ff1baae35a6cd25af97eb637
qianzf <qianzf@tenzont.com>
2018-05-26 20:38:11 +0800

    购买商品bug

--- src/main/resources/static/js/buy-bussiness.js
+++ src/main/resources/static/js/buy-bussiness.js
@@ -2,6 +2,11 @@ var $bussinesses_content = $('#bussinesses_content')
 
 var bindAll = function () {
     queryBussinessesInfo()
+
+    $('body').on('click', '.bussiness-item', function () {
+        var commodityId = $(this).attr('commodity_id')
+        window.location.href = '/commodity-details?commodityId='+commodityId
+    })
 }
 
 var queryBussinessesInfo = function () {
@@ -13,16 +18,16 @@ var queryBussinessesInfo = function () {
 
             for (var i=0;i<data.length;i++) {
                 var commodityId = data[i].commodityId
-                var tmp = '<div class="bussiness-item">\n' +
+                var tmp = '<div class="bussiness-item" commodity_id="'+commodityId+'">\n' +
                     '            <img src="'+data[i].imageURL+'" class="bussiness-image" width="100px" height="64px" />\n' +
                     '            <div class="bussiness-name" style="margin-top: 6px;margin-left: 18px;">'+data[i].commodityName+'</div>\n' +
                     '            <div class="bussiness-price" style="margin-left: 18px;"><span>￥</span><span class="bussiness-price-number">'+data[i].price+'</span></div>\n' +
                     '        </div>'
 
                 var $item = $(tmp)
-                $item.on('click', function () {
-                    window.location.href = '/commodity-details?commodityId='+commodityId
-                })
+                // $item.on('click', function () {
+                //     window.location.href = '/commodity-details?commodityId='+commodityId
+                // })
                 $bussinesses_content.append($item)
             }
 
